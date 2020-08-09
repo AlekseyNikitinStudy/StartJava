efd0a89eca9059a07f3caef8eeb066be7ba513bc
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-08-09 23:05:27 +0400

    Create GuessTheNumber.java

---
+++ Lesson1/GuessTheNumber.java
@@ -0,0 +1,20 @@
+public class GuessTheNumber {
+    public static void main (String[] args) {
+        int number = 34;
+        int playerNumber = 50;
+        System.out.println("Try to guess the number.");
+        do {
+            System.out.println("Player number is " + playerNumber);
+            if (playerNumber > number) {
+                System.out.println("The number is less than player number " + playerNumber);
+                playerNumber = playerNumber - 1;
+            }
+            else
+                if (playerNumber < number){
+                    System.out.println("The number is more than player number " + playerNumber);
+                    playerNumber = playerNumber + 1;
+                }
+        } while (playerNumber != number);
+        System.out.print("The number is equals player number! The number is " + number);
+    }
+}
