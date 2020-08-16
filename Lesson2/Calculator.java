46263953ae66741a4654db74d3585831e839ce17
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-08-16 20:45:41 +0400

    Calculator classes add

---
+++ Lesson2/Calculator.java
@@ -0,0 +1,44 @@
+public class Calculator {
+    int x;
+    int y;
+    char sign;
+
+    public Calculator(int x, int y, char sign) {
+        this.x = x;
+        this.y = y;
+        this.sign = sign;
+    }
+
+    public void calculateAndOut() {
+        int resultInt;
+        float resultFloat;
+
+        switch (sign) {
+            case ('+'):
+                resultInt = x + y;
+                System.out.println(x + " + " + y + " = " + resultInt);
+                break;
+            case ('*'):
+                resultInt = x * y;
+                System.out.println(x + " * " + y + " = " + resultInt);
+                break;
+            case ('/'):
+                resultFloat = (float) x / (float) y;
+                System.out.println(x + " / " + y + " = " + resultFloat);
+                break;
+            case ('^'):
+                resultInt = 1;
+                for (int i = 0; i < y; i++) {
+                    resultInt *= x;
+                }
+                System.out.println(x + " ^ " + y + " = " + resultInt);
+                break;
+            case ('%'):
+                resultInt = x % y;
+                System.out.println(x + " % " + y + " = " + resultInt);
+                break;
+            default:
+                System.out.println("Недопустимый знак.");
+        }
+    }
+}
