5f2a7b21879ef0bae8d53087137a9ab82d2d7be7
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-08-16 20:48:30 +0400

    Update WolfModified.java

--- Lesson2/WolfModified.java
+++ Lesson2/WolfModified.java
@@ -1,9 +1,57 @@
 public class WolfModified {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String gender;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     private float height;
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
     private float weight;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
     private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорретный возраст");
+        } else {
+            this.age = age;
+        }
+    }
 
     void walk() {
         System.out.println(name + " гуляет.");
@@ -23,49 +71,5 @@ public class WolfModified {
 
     void hunt() {
         System.out.println(name + " охотится.");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public void setAge(int age) {
-        if (age > 8) {
-            System.out.println("Некорретный возраст");
-        } else {
-            this.age = age;
-        }
     }
 }
