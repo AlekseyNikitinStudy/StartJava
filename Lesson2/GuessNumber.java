881ea7ee35b58f84c71f718ce253a474e0101511
Aleksey Nikitin <69401560+AlekseyNikitinStudy@users.noreply.github.com>
2020-08-27 10:57:11 +0300

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -13,11 +13,20 @@ public class GuessNumber {
 
     public void startGame() {
         generateHiddenNumber();
-        Player currentPlayer = playerTwo;
+        Player currentPlayer = null;
+        /* Игровой цикл:
+         1. Определяем текущего игрока.
+         2. Принимает ввод номера от него.
+         3. Показываем подсказку
+         4. Принимаем решение продолжении в зависимости угадано число или нет. */
         do {
+            /* При первой итерации цикла currentPlayer = playerOne (благодаря инициализации currentPlayer = null выше).*/
             currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+            /* Ввод номер от текущего игрока. */
             inputNumber(currentPlayer);
+            /* Показываем подсказку - больше, меньше или победа. */
             showHint(currentPlayer);
+        /* Сравниваем на равенство с загаданным числом и принимаем решение продолжать или нет цикл. */
         } while (!compareNumber(currentPlayer));
 
         System.out.println("Игрок " + currentPlayer.getName() + " победил!");
@@ -35,9 +44,9 @@ public class GuessNumber {
     }
 
     private void showHint(Player player) {
-        System.out.println(player.getNumber() > hiddenNumber ? "Загаданное число меньше."
-                : player.getNumber() < hiddenNumber ? "Загаданное число больше."
-                : "Число угадано!");
+        if (!compareNumber(player))
+            System.out.println(player.getNumber() > hiddenNumber ? "Загаданное число меньше."
+                : "Загаданное число больше.");
     }
 
     private boolean compareNumber(Player player) {
