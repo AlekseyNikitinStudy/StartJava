026dea0aa20c6e7147c3142c45dd61d0881d71e0
Aleksey Nikitin <69401560+AlekseyNikitinStudy@users.noreply.github.com>
2020-09-03 11:14:36 +0300

    Update Player.java

--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -3,53 +3,42 @@ package com.startjava.lesson_2_3_4.game;
 import java.util.Arrays;
 
 public class Player {
-    public static final int MAX_ATTEMPTS = 10;
-
     private String name;
     private int[] numbers;
-    private int currentIndex;
+    private int attempt;
 
     public Player(String name) {
         this.name = name;
-        numbers = new int[MAX_ATTEMPTS];
+        numbers = new int[GuessNumber.MAX_ATTEMPTS];
     }
 
     public String getName() {
         return name;
     }
 
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public int getAttempt() {
+        return attempt;
+    }
+
     public int getCurrentNumber() {
-        return numbers[currentIndex - 1];
+        return numbers[attempt - 1];
     }
 
     public void setCurrentNumber(int number) {
-        this.numbers[currentIndex] = number;
-        currentIndex++;
+        numbers[attempt] = number;
+        attempt++;
     }
 
-    public void startGame() {
-        Arrays.fill(numbers, 0, currentIndex, 0);
-        currentIndex = 0;
+    public void clearNumbers() {
+        Arrays.fill(numbers, 0, attempt, 0);
+        attempt = 0;
     }
 
     public boolean isAttemptsOver() {
-        return currentIndex == MAX_ATTEMPTS;
-    }
-
-    public void showNumbers() {
-        System.out.print("\nЧисла игрока " + name + ": ");
-        int[] filledNumbers = Arrays.copyOf(numbers, currentIndex);
-
-        for (int filledNumber : filledNumbers) {
-            System.out.print(filledNumber + " ");
-        }
-    }
-
-    public void showResult(boolean isVictory) {
-        if (isVictory) {
-            System.out.println("\nИгрок " + name + " угадал число " + numbers[currentIndex - 1] + " с " + currentIndex + " попытки");
-        } else {
-            System.out.println("\nУ " + name + " закончились попытки.");
-        }
+        return attempt == GuessNumber.MAX_ATTEMPTS;
     }
 }
