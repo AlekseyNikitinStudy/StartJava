666ea7155e58e74b84580749abc04e583481bf61
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-09-15 23:22:48 +0400

    Fix mistakes

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,13 +3,14 @@ CREATE DATABASE jaegers;
 
 \c jaegers;
 
-CREATE TABLE jaegers (
-id INTEGER NOT NULL,
-modelName VARCHAR(100),
-mark VARCHAR(100),
-height REAL,
-weight REAL,
-status VARCHAR(100),
-origin VARCHAR(100),
-launch TIMESTAMP,
-kaijuKill INTEGER);
+CREATE TABLE jaegers(
+    id INTEGER NOT NULL,
+	modelName VARCHAR(100),
+	mark VARCHAR(100),
+	height REAL,
+	weight REAL,
+	status VARCHAR(100),
+	origin VARCHAR(100),
+	launch TIMESTAMP,
+	kaijuKill INTEGER
+);
